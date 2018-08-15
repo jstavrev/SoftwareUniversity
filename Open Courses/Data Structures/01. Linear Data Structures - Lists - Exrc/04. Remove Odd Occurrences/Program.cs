9941f26505159691cc064cb47f98a73dfce3a1e8
Ioan Stavrev <j.stavrev@yahoo.com>
2018-08-15 19:18:18 +0300

    - linear data structures lists exercise tasks

---
+++ Open Courses/Data Structures/01. Linear Data Structures - Lists - Exrc/04. Remove Odd Occurrences/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Remove_Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            foreach (var num in input)
+            {
+                if (dict.ContainsKey(num))
+                {
+                    dict[num]++;
+                }
+                else
+                {
+                    dict[num] = 1;
+                }
+            }
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (dict[input[i]] % 2 != 0)
+                {
+                    input.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", input));
+        }
+    }
+}
