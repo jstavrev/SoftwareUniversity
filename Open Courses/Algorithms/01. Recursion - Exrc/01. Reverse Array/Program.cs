540fef7a24f51ab6f82f356befdb9dee462010f0
Ioan Stavrev <j.stavrev@yahoo.com>
2018-09-08 18:39:31 +0300

    - recursion exercise tasks

---
+++ Open Courses/Algorithms/01. Recursion - Exrc/01. Reverse Array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01._Reverse_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            var result = new string[arr.Length];
+
+            int index = arr.Length;
+            ReverseArray(arr, result, index);
+
+            Console.WriteLine(String.Join(" ", result));
+        }
+
+        public static void ReverseArray(string[] arr, string[] result, int index)
+        {
+            if (index < 0)
+            {
+                return;
+            }
+
+            ReverseArray(arr, result, index - 1);
+
+            if (index == arr.Length)
+            {
+                return;
+            }
+
+            result[index] = arr[arr.Length - (index + 1)];
+        }
+    }
+}
---
+++ Open Courses/Algorithms/01. Recursion - Exrc/03. Combinations with Repetitions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _03._Combinations_with_Repetitions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            int[] array = new int[k];
+
+            int index = 0;
+            Solve(array, index, n);
+        }
+
+        private static void Solve(int[] array, int index, int n, int element = 1)
+        {
+            if (index == array.Length)
+            {
+                Console.WriteLine(String.Join(" ", array));
+                return;
+            }
+
+            for (int i = element; i <= n; i++)
+            {
+                    array[index] = i;
+
+                Solve(array, index + 1, n, i);
+            }
+        }
+    }
+}
